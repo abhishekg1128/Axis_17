779cfeab934a68a6c43ee7bd492dfa9e4126df88
abhishekg1128 <abhishekg1128@yahoo.com>
2016-12-20 14:43:05 +0530

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	setTimeout(function(){
+		$('#loaded').addClass('loaded');
+		// $('h1').css('color','#222222');
+	}, 0);
+
+	// $('#loader-wrapper').css({visibility: 'hidden'})
+
+});
